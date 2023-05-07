f455125c37d24dcb986064899fbf24f0f9df18c7
Ultra-Code <mega.alpha100@gmail.com>
2023-05-07 22:09:42 +0000

    Misc improvements

--- src/main.zig
+++ src/main.zig
@@ -13,26 +13,29 @@ pub fn main() !void {
 
     var address_info = AddressInfo{
         .data = .{
-            .ai_family = cnet.AF_UNSPEC,
-            .ai_socktype = cnet.SOCK_STREAM,
+            .ai_family = cnet.AF_UNSPEC, //support both IPv4 or IPv6
+            .ai_socktype = cnet.SOCK_STREAM, //TCP stream sockets
             .ai_protocol = undefined,
             .ai_addrlen = undefined,
             .ai_addr = undefined,
             .ai_canonname = undefined,
             .ai_next = undefined,
+            //AI_PASSIVE means to assign address of localhost to socket
             .ai_flags = undefined,
         },
     };
 
+    const service_name_or_port = "https";
+    //network address and service translation
     address_info.getaddrinfo_status = cnet.getaddrinfo(
         program_arg,
-        "https",
+        service_name_or_port,
         &address_info.data,
         &address_info.serviceinfo,
     );
     defer cnet.freeaddrinfo(address_info.serviceinfo);
 
-    if (address_info.getaddrinfo_status != 0) {
+    if (address_info.getaddrinfo_status != SUCCESS) {
         std.log.err(
             "getaddrinfo error: {s}",
             .{cnet.gai_strerror(address_info.getaddrinfo_status)},
@@ -65,7 +68,7 @@ pub fn main() !void {
             if (ip_str) |ip| {
                 std.debug.print(" {s}: {s}\n", .{ "IPV4", ip });
             } else {
-                std.log.err("{s}", .{cnet.strerror(std.c._errno().*)});
+                std.log.err("inet_ntop failed:{s}", .{cnet.strerror(std.c._errno().*)});
             }
         } else {
             var ip_buf: [cnet.INET6_ADDRSTRLEN]u8 = undefined;
@@ -86,7 +89,7 @@ pub fn main() !void {
             if (ip_str) |ip| {
                 std.debug.print(" {s}: {s}\n", .{ "IPV6", ip });
             } else {
-                std.log.err("{s}", .{cnet.strerror(std.c._errno().*)});
+                std.log.err("inet_ntop failed:{s}", .{cnet.strerror(std.c._errno().*)});
             }
         }
     }
